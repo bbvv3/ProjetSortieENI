10f025094dd0980c398380f12257cdcf2f568489
Goulven <goulven.trubuil2022@campus-eni.fr>
2022-09-14 13:27:49 +0200

    Affichage partiel des sorties (maj)

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Form\MainType;
+use App\Repository\SortieRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -16,15 +17,22 @@ class MainController extends AbstractController
     /**
      * @Route("", name="_home")
      */
-    public function rechercher(Request $request): Response
+    public function home(Request $request, SortieRepository $sortieRepository): Response
     {
         $mainForm = $this->createForm(MainType::class);
 
+        $sorties = $sortieRepository->findAll();
+        //$etats =  $etatRepository->findAll();
+        //$query = "SELECT id, nom
+        //            FROM etat
+        //            INNER JOIN sortie
+        //            ON etat.libelle = sortie.id"
+
         return $this->render('home/home.html.twig', [
-           // 'controller_name' => 'MainController',
-            'mainForm'=>$mainForm->createView()
+            // 'controller_name' => 'MainController',
+            'mainForm'=>$mainForm->createView(),
+            "sorties" => $sorties,
+            //"etats" => $etats,
         ]);
     }
-
-
 }
