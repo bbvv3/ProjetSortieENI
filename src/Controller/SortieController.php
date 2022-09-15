6a8f95db79eb65c10ab76593f80ae1af3be9c01c
Robert <bobi25@hotmail.fr>
2022-09-15 14:32:10 +0200

    lien vers modif

--- src/Controller/SortieController.php
+++ src/Controller/SortieController.php
@@ -5,19 +5,15 @@ namespace App\Controller;
 
 
 use App\Entity\Sortie;
-use App\Form\ModifierSortieType;
 use App\Form\SortieType;
 use App\Repository\EtatRepository;
-use App\Repository\LieuRepository;
 use App\Repository\SortieRepository;
-use Doctrine\ORM\EntityManager;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\Form;
-use Symfony\Component\Security\Core\User\User;
+
 
 class SortieController extends AbstractController
 {
@@ -27,48 +23,40 @@ class SortieController extends AbstractController
     public function creationSortie(int                    $id,
                                    Request                $request,
                                    SortieRepository       $sortieRepository,
-                                   LieuRepository         $lieuRepository,
                                    EntityManagerInterface $entityManager,
                                    EtatRepository         $etatRepository): Response
     {
 
-        $creerSortie = new Sortie();
-
-
+        if($id !=0)
+        {
+            $creerSortie=$sortieRepository->find($id);
+        }else{
+            $creerSortie=new Sortie();
+        }
         $sortieForm = $this->createForm(SortieType::class, $creerSortie);
         $sortieForm->handleRequest($request);
 
         if ($sortieForm->isSubmitted() && $sortieForm->isValid()) {
-
             if ($sortieForm->getClickedButton() === $sortieForm->get('publier')) {
-                $etat = $etatRepository->findOneBy(['libelle' => 'En cours']);
+                $etat = $etatRepository->findOneBy(['libelle' => 'Ouverte']);
                 $creerSortie->setEtatSortie($etat);
                 $entityManager->persist($creerSortie);
-
-
             } else if ($sortieForm->getClickedButton() === $sortieForm->get('enregistrer')) {
                 $etat = $etatRepository->findOneBy(['libelle' => 'En création']);
                 $creerSortie->setEtatSortie($etat);
                 $entityManager->persist($creerSortie);
-
             } else {
                 $sortieForm->getClickedButton() === $sortieForm->get('delete');
                 $entityManager->remove($creerSortie);
-
-
             }
+
             $entityManager->flush();
-            return $this->redirectToRoute('app_home', ['id' => $creerSortie->getId()]);
-
-
-
-
+            return $this->redirectToRoute('app_home');
         }
 
         return $this->render('creerSortie/creerSortie.html.twig', [
             'id' => $id,
             'sortieForm' => $sortieForm->createView(),
-
         ]);
     }
 
